11bfb62dfaf333133b8983c42b5d33f9fbda9336
insomnius <awc.aliana@gmail.com>
2025-04-20 16:09:21 +0700

    reworking g analytics

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -1,34 +1,19 @@
 'use client';
 
 import Script from 'next/script';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
 
 // Google tag ID
 const GA_MEASUREMENT_ID = 'G-JTX2K1RJ8N';
 
 export default function GoogleAnalytics() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    if (pathname && window.gtag) {
-      // Send pageview with updated path
-      window.gtag('event', 'page_view', {
-        page_path: pathname + (searchParams?.toString() ? `?${searchParams.toString()}` : ''),
-      });
-    }
-  }, [pathname, searchParams]);
-
   return (
     <>
-      {/* Google tag (gtag.js) - load the script */}
+      {/* Google tag (gtag.js) */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
       />
 
-      {/* Google Analytics configuration script */}
       <Script
         id="google-analytics"
         strategy="afterInteractive"
@@ -37,9 +22,7 @@ export default function GoogleAnalytics() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname + window.location.search
-            });
+            gtag('config', '${GA_MEASUREMENT_ID}');
           `,
         }}
       />
